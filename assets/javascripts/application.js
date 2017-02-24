c0b311a262ae090d8e1b448f304e8c651ff87cb5
Mike Hurl <mikehurl@gmail.com>
2017-02-24 10:38:19 +0000

    script to automatically switch between two images for each individual coach tip

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,17 +1,17 @@
-// var myIndex = 0;
-// carousel();
-//
-// function carousel() {
-//     var i;
-//     var x = document.getElementsByClassName("coach-image");
-//     for (i = 0; i < x.length; i++) {
-//        x[i].style.display = "none";
-//     }
-//     myIndex++;
-//     if (myIndex > x.length) {myIndex = 1}
-//     x[myIndex-1].style.display = "block";
-//     setTimeout(carousel, 2000);
-// };
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("coach-tip-image");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 1000);
+};
 
 var slideIndex = 1;
 showSlides(slideIndex);
